6f861e0457471af1be901a356dbb1fc5c4076b7d
Sławomir Kmiecik <33656921+Kmiecis@users.noreply.github.com>
2021-01-03 15:33:22 +0100

    Common

---
+++ Common/CommandsSystemBase.cs
@@ -0,0 +1,30 @@
+﻿using Unity.Entities;
+
+namespace CommonECS.Systems
+{
+	public abstract class CommandsSystemBase : SystemBase
+	{
+		protected BeginInitializationEntityCommandBufferSystem m_CommandBufferSystem;
+
+		protected override void OnCreate()
+		{
+			base.OnCreate();
+			m_CommandBufferSystem = World.GetOrCreateSystem<BeginInitializationEntityCommandBufferSystem>();
+		}
+
+		protected EntityCommandBuffer CreateCommandBuffer()
+		{
+			return m_CommandBufferSystem.CreateCommandBuffer();
+		}
+
+		protected EntityCommandBuffer.ParallelWriter CreateParallelWriter()
+		{
+			return m_CommandBufferSystem.CreateCommandBuffer().AsParallelWriter();
+		}
+
+		protected void FinalizeDependency()
+		{
+			m_CommandBufferSystem.AddJobHandleForProducer(this.Dependency);
+		}
+	}
+}
