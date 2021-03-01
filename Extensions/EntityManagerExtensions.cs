312750d7cb2ec3d65c038e05def31f57f8515120
Sławomir Kmiecik <33656921+Kmiecis@users.noreply.github.com>
2021-03-01 05:41:32 +0100

    Sorted extensions

---
+++ Extensions/EntityManagerExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Entities;
+
+namespace CommonECS
+{
+	public static class EntityManagerExtensions
+	{
+		public static void EnsureComponentData<T>(this EntityManager entityManager, Entity entity, T componentData)
+			where T : struct, IComponentData
+		{
+			if (entityManager.HasComponent<T>(entity))
+				entityManager.SetComponentData(entity, componentData);
+			else
+				entityManager.AddComponentData(entity, componentData);
+		}
+	}
+}
---
+++ Extensions/NativeArrayExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Unity.Collections;
+
+namespace CommonECS
+{
+	public static class NativeArrayExtensions
+	{
+		public static NativeArray<T> Populate<T>(this NativeArray<T> self, T value)
+			where T : struct
+		{
+			for (int i = 0; i < self.Length; ++i)
+				self[i] = value;
+			return self;
+		}
+
+		public static bool TryIndexOf<T, U>(this NativeArray<T> self, U value, out int index)
+			where T : struct, IEquatable<U>
+		{
+			index = self.IndexOf(value);
+			return index != -1;
+		}
+	}
+}
